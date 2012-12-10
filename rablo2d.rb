b52974b9e11cfeb3ca7af91bbd28a7ef96460d87
Chong <chong@ubuntu.(none)>
2012-12-10 17:34:20 -0500

    clean up code

--- rablo2d.rb
+++ rablo2d.rb
@@ -13,24 +13,10 @@ class Field
   
   def initialize(app, points, sreps, shifts)
     @app = app
-    @field = []
     # set window size
     @width, @height= 800, 600
-
-    @offset = [(@app.width - @width.to_i) / 2, (@app.height - @height.to_i) / 2]
-    @fullpoints = points
     @sreps = sreps 
     @shifts = shifts
-    @colorLst = ["#66CCFF"]
-    (@sreps.length-1).times do |i|
-      color = ("66CCFF"[0..1].to_i(16) + (i + 1) *  20).to_s(16) + ("66CCFF"[2..3].to_i(16) - (i+1)*20).to_s(16) + ("66CCFF"[4..5].to_i(16) - (i + 1) * 50).to_s(16)
-      color = "#" + color.to_s
-      @colorLst << color
-    end
-  end
-  
-  def setPoints(points)
-    @fullpoints = points
   end
   
   def setSrep(srep, index) 
@@ -40,30 +26,14 @@ class Field
   def addSrep(srep) 
     @sreps << srep
   end
-  
-  def setShift(shift, index)
-    @shifts[index] = shift
-  end
-
-  def addShift(shift)
-    @shifts << shift
-  end
-   
+ 
   def render_point_big(x, y, color)
     @app.stroke color
     @app.strokewidth 2
     @app.nofill
     @app.oval x-1,y-1, 2
   end
-
-  def render_figure(points, shiftx, shifty, color, is_big, scale)
-    if is_big
-      points.each_slice(4) do |point|
-        render_point_big(scale* (point[0][0] + shiftx), scale * (point[0][1] + shifty), color)
-      end
-    end
-  end
-  
+ 
   def render_circle(cx, cy, d, color)
     @app.stroke color
     @app.nofill 
@@ -71,7 +41,6 @@ class Field
   end
 
   def render_spokes(cx, cy, type, spoke_length, spoke_direction, color)
-
  # white: spoke zero
  # black: spoke 1
     @app.stroke color
@@ -94,10 +63,9 @@ class Field
     srep = args[0]
     shiftx  = args[1]
     shifty = args[2]
-    scale = args[4]
-    show_sphere = args[5]
+    scale = args[3]
+    show_sphere = args[4]
       
-
     srep.atoms.each_with_index do |atom|
 
       render_atom(atom.x + shiftx, atom.y + shifty, atom.corresponding_color)
@@ -142,9 +110,7 @@ class Field
     xs = gamma_file.gets.split(" ").collect{|x| x.to_f}
     ys = gamma_file.gets.split(" ").collect{|y| y.to_f}
    
-
     if (srep.interpolate_finished)
-    
       xs.each_with_index do |x,i|
       spoke_ind = i*2
       linkingIndex = srep.getExtendInterpolatedSpokesEnd()[spoke_ind][2]
@@ -160,7 +126,6 @@ class Field
         color2 = sreps[linkingIndex].color
       end
       render_atom(x+shiftx,ys[i]+shifty, color1)
-      
       render_atom(x+shiftx+1, ys[i]+shifty-1, color2)
     end
     else 
@@ -180,13 +145,13 @@ class Field
    
   def render_extend_interp_spokes(shiftx, shifty, color, ibegin, iend)
     @app.stroke color
-     iend.each_with_index do |p, i|
+    iend.each_with_index do |p, i|
       @app.line(ibegin[i][0]+shiftx, ibegin[i][1]+shifty, p[0]+shiftx, p[1]+shifty)
     end
   end
   
   def render_atom(x, y, color)
-      render_point_big(x, y, color)
+    render_point_big(x, y, color)
   end
   
   def render_linking_structure(shifts)
@@ -201,7 +166,7 @@ class Field
     checkSrepIntersection
 
     $sreps.each.with_index do |srep, i|
-      render_srep(srep, @shifts[i] , @shifts[i] , @colorLst[i], 1.5, true)
+      render_srep(srep, @shifts[i] , @shifts[i] , 1.5, true)
     end
 
     if $show_linking_structure 
@@ -230,66 +195,6 @@ class Field
    cell_exists?(x, y) ? @field[y][x] = v : false
  end
 end
-
-class DrawPad
-  attr_reader :atoms 
-	
-  def initialize(app)
-    @app = app
-    @atoms = []
-    @inCircle = false
-  end
-	
-  def paint()
-    @app.clear
-    @app.background 'pat101_21.png'
-    @app.flow :margin => 3 do
-      @fileName = @app.edit_line
-      @app.button("Save") {
-        alert @atoms.to_s
-        if @fileName.text != ""
-          fn = @fileName.text
-        else 
-          fn = "srep_temp"
-        end
-        File.open(fn + '.txt','w') {|file|
-	  file.write (@atoms.to_s)
-	  file.close}
-      }
-      @app.button("Undo") {
-        if(@atoms.size > 0)
-	  @atoms.pop
-        end
-        @inCircle = false
-	  refresh }
-     end
-     @app.nofill
-     atoms.each do |atom|
-       @app.oval atom[0]-atom[2]/2, atom[1]-atom[2]/2, atom[2]
-     end
-   end
-
-   def refresh()
-     paint
-     @app.click do |button, left, top|
-       if @inCircle == false
-	 @atoms << [left, top, 10]
-	 @inCircle = true
-	 paint
-       else 
-	 @atoms.last[2] = @atoms.last[2] + 1.5
-	 paint
-       end
-   end
-   @app.keypress do |k|
-     if "#{k.inspect}".to_s == "g"
-     else
-       @inCircle = false
-     end
-   end
- end
-	
-end 
 
 class InterpolateControl
 	
@@ -375,12 +280,6 @@ Shoes.app :width => 1000, :height => 800, :title => '2d multi object' do
     clear do
       background rgb(50, 50, 90, 0.7)
       flow :margin => 6 do
-#	button("Srep Image"){
-#	  window :title => "draw srep", :width => 402, :height => 375 do
-#	    dp = DrawPad.new self
-#	    dp.refresh
-#         end
-#	}
 
         button("Dilate") { 
 	  $sreps.each do |srep|
@@ -405,52 +304,6 @@ Shoes.app :width => 1000, :height => 800, :title => '2d multi object' do
 	  initialConfig
 	}
 
-	button("Move") {
-	  @shifts[0] = @shifts[0] + 10
-	  refresh @points, $sreps, @shifts
-	}
-
-	button("Rotate") {
-	  rotateSrep(@sreps[0], Math::PI/6)
-	  refresh @points, $sreps, @shifts
-	}
-
-	button("Link") {
-	  flag = 0
-	  count = 0
-	  while flag == 0
-	    @sreps.each.with_index do |srep, j|
-	      if j == 0
-	      else 
-		srep.each.with_index do |a, i|
-		  @field.totalCorreLst.size.times do |k|
-		    if @field.totalCorreLst[k][1] == i
-		      @dontGrowLst << [j,i]
-		      break
-		    elsif !( @dontGrowLst.include? [j,i] )
-		      a[2][0] = a[2][0] * 1.05
-		      break
-		    end
-		  end
-		end
-	      end
-	    end
-	    refresh @points, $sreps, @shifts
-	    count = @field.getHowManyColored
-	    if count >= 7
-	      flag = 1
-	    end
-          end
-	}
-
-#	button("Save") {
-#	  fileName = 'pat101_31_par_link.txt'
-#	  File.open(fileName,'w') {|file|
-#	    file.write (@field.totalCorreLst.to_s)
-#	    file.close
-#         }
-#	}
- 
         button("Check r-k File") {
           window :title => "draw srep", :width => 402, :height => 375 do
 	    dp = InterpolateControl.new(self)
@@ -632,10 +485,7 @@ Shoes.app :width => 1000, :height => 800, :title => '2d multi object' do
      stack do @status = para :stroke => black end
      @field.paint
      para "\n"
-    # para "mask_func[0]: "  + $sreps[0].extend_interpolated_spokes_end.collect{|e| e[2]}.to_s
-    # para "mask_func[1]: "  + $sreps[1].extend_interpolated_spokes_end.collect{|e| e[2]}.to_s
-    # para "mask_func[2]: "  + $sreps[2].extend_interpolated_spokes_end.collect{|e| e[2]}.to_s
-      para $info
+     para $info
    end  
  end
   
